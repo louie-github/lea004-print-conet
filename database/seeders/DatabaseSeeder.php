dad5a426a5002e82ab3fe8bc79c3365698759674
Louie Torres <lbtowers@proton.me>
2024-04-02 11:22:45 +0800

    feat: Add a test user when seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Document;
 use App\Models\Price;
 use App\Models\Transaction;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -27,9 +28,18 @@ class DatabaseSeeder extends Seeder
             'password' => bcrypt('secret')
         ]);
 
+        // Manually create a test user
+        DB::table('users')->insert([
+            'username' => 'test',
+            'firstname' => 'Test',
+            'lastname' => 'Person',
+            'email' => 'test@test.com',
+            'is_admin' => false,
+            'password' => bcrypt('password')
+        ]);
+
         // Document::factory()->count(10)->create();
 
         Price::factory()->create();
-        
     }
 }
