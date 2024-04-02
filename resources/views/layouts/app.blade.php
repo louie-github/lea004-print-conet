c4b39ec6cd52530a6cbf30ca1410dc3c3d58292b
Louie Torres <lbtowers@proton.me>
2024-04-03 03:27:06 +0800

    fix: Fix typo in website title (PRING)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <link rel="apple-touch-icon" sizes="76x76" href="/img/apple-icon.png">
     <link rel="icon" type="image/png" href="/img/favicon.png">
     <title>
-        PRING CO-NET WEB
+        PRINT CO-NET WEB
     </title>
     <!--     Fonts and icons     -->
     <link href="{{asset('https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700')}}" rel="stylesheet" />
