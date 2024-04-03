6331617c5943064c2b7585bc3e415fefe7231e77
Louie Torres <lbtowers@proton.me>
2024-04-03 08:02:12 +0800

    feat: Auto-expire transactions every hour

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,8 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class Kernel extends ConsoleKernel
 {
@@ -13,6 +15,16 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+
+        // Automatically expire transactions older than 15 minutes
+        // every hour.
+        $schedule->call(function() {
+            DB::transaction(function() {
+                DB::table('transactions')
+                    ->where('updated_at', '<', Carbon::now()->subMinutes(15))
+                    ->update(['status' => 'Expired']);
+            });
+        })->hourly();
     }
 
     /**
