8da69c36181d46f977b3e24fdadce9b0d2147434
Louie Torres <lbtowers@proton.me>
2024-04-13 16:16:58 +0800

    feat: Use Office Online as web viewer

--- resources/views/components/file-iframe.blade.php
+++ resources/views/components/file-iframe.blade.php
@@ -1,12 +1,10 @@
 @php
-$ext = pathinfo($document->url, PATHINFO_EXTENSION);
-$url = storage_path('public/' . $document->url);
-$url = env('KIOSK_BASE') . \Illuminate\Support\Facades\Storage::url($document->url);
+    $ext = pathinfo($document->url, PATHINFO_EXTENSION);
+    $url = env('KIOSK_BASE') . \Illuminate\Support\Facades\Storage::url($document->url);
 @endphp
 @if ($ext === 'pdf')
-<iframe class="mb-5" src="{{ route('pdf.viewer', ['id' => $document->id]) }}" width="100%"
-    height="620px"></iframe>
+    <iframe class="mb-5" src="{{ route('pdf.viewer', ['id' => $document->id]) }}" width="100%" height="620px"></iframe>
 @else
-<iframe width="100%" height="620px" class="doc"
-    src="https://docs.google.com/gview?url={{ url($url) }}&embedded=true"></iframe>
+    <iframe width="100%" height="620px" class="doc"
+        src="https://view.officeapps.live.com/op/embed.aspx?src={{ url($url) }}"></iframe>
 @endif
